2d12bf731c6aa9d6c0853da9c6c0979557cdee80
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-28 15:08:15 +0200

    modify packagejson

---
+++ READ.ME
@@ -0,0 +1,1 @@
+https://bulletinboardkodilla.herokuapp.com/
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
     // "origin": "http://localhost:3000",
 }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 /* API ENDPOINTS */
 app.use('/api', postsRoutes);
